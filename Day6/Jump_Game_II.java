1c8691a1fcd1d11d04a57a0407e909ac93fa68c3
Madhu <madhu2056.be23@chitkara.edu.in>
2026-07-13 09:50:07 +0530

    Jump Game II solution

---
+++ Day6/Jump_Game_II.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public static int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(jump(nums));
+
+        sc.close();
+    }
+}
